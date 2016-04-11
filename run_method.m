6a05beb087f6e24e534cdc4975e990434031cc00
Goran Flegar <flegar.goran@gmail.com>
2016-04-11 14:12:44 +0200

    Improve test scripts

---
+++ run_method.m
@@ -0,0 +1,28 @@
+function [y] = run_method(name, test, n)
+
+h = (test.e - test.s) / n;
+switch name
+case 'euler'
+    y = euler(test.s, test.sol(test.s), test.fn, h, n);
+case 'meuler'
+    y = enhanced_euler(test.s, test.sol(test.s), test.fn, h, n);
+case 'kutta'
+    K = [0 0 0 0; 0.5 0.5 0 0; 1 -1 2 0; 0 1/6 2/3 1/6];
+    y = rk(K, test.s, test.sol(test.s), test.fn, h, n);
+case 'heun'
+    H = [0 0 0 0; 1/3 1/3 0 0; 1 0 2/3 0; 0 1/4 0 3/4];
+    y = rk(H, test.s, test.sol(test.s), test.fn, h, n);
+case 'trap'
+    y = trapezoid(test.s, test.sol(test.s), test.A, h, n);
+case 'ab4'
+    ab4 = 1/24 * [-9; 37; -59; 55];
+    y = multistep(ab4, test.s, test.sol(test.s), test.fn, h, n);
+case 'ab5'
+    ab5 = 1/720 * [251; -1274; 2616; -2774; 1901];
+    y = multistep(ab5, test.s, test.sol(test.s), test.fn, h, n);
+case 'solution'
+    interval = num2cell(test.s:h:test.e);
+    ycell = cellfun(test.sol, interval, 'UniformOutput', false);
+    y = cell2mat(ycell);
+end
+
--- test_methods.m
+++ test_methods.m
@@ -1,108 +1,38 @@
-function test_methods(n, t)
-switch t
-    case 1
-        s = 1;
-        e = 10;
-        A = [];
-        fn = @(t, y) -y/(t^2);
-        sol = @(t) exp(1./t);
-    case 2
-        s = 1;
-        e = 5;
-        A = [];
-        fn = @(t, y) 2*t*y;
-        sol = @(t) exp(t.^2);
-    case 3
-        s = 0.1;
-        e = 1;
-        A = [];
-        fn = @(t, y) -y / (t^2 * tan(1/t));
-        sol = @(t) sin(1./t);
-    case 4
-        s = 1;
-        e = 10;
-        A = 1;
-        fn = @(t, y) A*y;
-        sol = @(t) exp(t);
-    case 5
-        s = 0;
-        e = 1;
-        A = [];
-        fn = @(t, y) y*tan(t) + 1/cos(t);
-        sol = @(t) t ./ cos(t);
-    case 6
-        s = 0;
-        e = 5;
-        A = [3 5; -2 -8];
-        fn = @(t, y) A * y;
-        sol = @(t) [5*exp(2*t) - 3*exp(-7*t); -exp(2*t) + 6*exp(-7*t)];
-    case 7
-        s = 0;
-        e = 10;
-        A = [0 -2; 2 0];
-        fn = @(t, y) A * y + [3; -2*t];
-        C = [1; 1];
-        sol = @(t) [C(1)*cos(2*t) + C(2)*sin(2*t) + t; C(1)*sin(2*t) - C(2)*cos(2*t) + 1];
+function test_methods(n, t, meths, styles)
+if ~exist('meths')
+    meths = {'euler', 'meuler', 'kutta', 'heun', 'ab4'};
 end
+
+if ~exist('styles')
+    styles = {'r', 'r--', 'g', 'g--', 'c'};
+end
+
+styles = styles(1:length(meths))
+
+meths{length(meths)+1} = 'solution';
+styles{length(styles)+1} = 'k';
+
+test = get_test(t);
+
+s = test.s;
+e = test.e;
 h = (e - s) / n;
+int = s:h:e;
 
-K = [0 0 0 0; 0.5 0.5 0 0; 1 -1 2 0; 0 1/6 2/3 1/6];
-H = [0 0 0 0; 1/3 1/3 0 0; 1 0 2/3 0; 0 1/4 0 3/4];
-
-y_e = euler(s, sol(s), fn, h, n);
-y_ee = enhanced_euler(s, sol(s), fn, h, n);
-y_k = rk(K, s, sol(s), fn, h, n);
-y_h = rk(H, s, sol(s), fn, h, n);
-y_t = trapezoid(s, sol(s), A, h, n);
-ab4 = 1/24 * [-9; 37; -59; 55];
-ab5 = 1/720 * [251; -1274; 2616; -2774; 1901];
-y_ab4 = multistep(ab4, s, sol(s), fn, h, n);
-y_ab5 = multistep(ab5, s, sol(s), fn, h, n);
-y_c = sol(s:h:e);
-
-if size(y_c, 1) == 1
-    plot(s:h:e, y_e, 'r');
-    hold on;
-    plot(s:h:e, y_ee, 'g');
-    plot(s:h:e, y_k, 'c');
-    %plot(s:h:e, y_h, 'k');
-    %plot(s:h:e, y_t, 'm');
-    plot(s:h:e, y_ab4, 'r--');
-    plot(s:h:e, y_ab5, 'g--');
-    hold on;
-    plot(s:h:e, y_c);
-    %legend('euler', 'enhanced euler', 'kutta', 'heun', 'trapezoid', 'ab4', 'real y');
-    legend('euler', 'enhanced euler', 'kutta', 'ab4', 'ab5', 'real y');
-else
-    subplot(1, 2, 1);
-    plot(s:h:e, y_e(1,:), 'r');
-    hold on;
-    plot(s:h:e, y_ee(1,:), 'g');
-    plot(s:h:e, y_k(1,:), 'c');
-    %plot(s:h:e, y_h(1,:), 'k');
-    %plot(s:h:e, y_t(1,:), 'm');
-    plot(s:h:e, y_ab4(1,:), 'r--');
-    plot(s:h:e, y_ab5(1,:), 'g--');
-    hold on;
-    plot(s:h:e, y_c(1,:));
-    %legend('euler', 'enhanced euler', 'kutta', 'heun', 'trapezoid', 'ab4', 'real y');
-    legend('euler', 'enhanced euler', 'kutta', 'ab4', 'ab5', 'real y');
-    subplot(1, 2, 2);
-    plot(s:h:e, y_e(2,:), 'r');
-    hold on;
-    plot(s:h:e, y_ee(2,:), 'g');
-    plot(s:h:e, y_k(2,:), 'c');
-    %plot(s:h:e, y_h(2,:), 'k');
-    %plot(s:h:e, y_t(2,:), 'm');
-    plot(s:h:e, y_ab4(2,:), 'r--');
-    plot(s:h:e, y_ab5(2,:), 'g--');
-    hold on;
-    plot(s:h:e, y_c(2,:));
-    %legend('euler', 'enhanced euler', 'kutta', 'heun', 'trapezoid', 'ab4', 'real y');
-    legend('euler', 'enhanced euler', 'kutta', 'ab4', 'ab5', 'real y');
+figure
+for i = 1:length(meths)
+    y = run_method(meths{i}, test, n);
+    sz = size(y, 1);
+    for j = 1:sz
+        subplot(sz, 1, j);
+        hold on;
+        plot(int, y(j,:), styles{i});
+    end
 end
 
-pause
-close
+for j = 1:sz
+    subplot(sz, 1, j);
+    legend(meths);
+end
 
 end
